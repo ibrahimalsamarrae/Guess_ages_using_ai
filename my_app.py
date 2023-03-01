4ac22f853e60e04d64aa6086b6bcf53beaebf68f
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-02 02:33:23 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 from deepface import DeepFace
-
+import cv2
 
 def main():
     st.header("Görünüşüne göre yaşını bil")
@@ -10,6 +10,7 @@ def main():
     file = st.file_uploader("Upload Photo")
     if file is not None:
         st.image(file)
+        img = cv2.imread(file)
         image = Image.open(file)
         image = tf.image.resize(image, [224,224]) 
         image = tf.keras.preprocessing.image.img_to_array(image)
@@ -20,7 +21,7 @@ def main():
       
         
         model = tf.keras.models.load_model("my_model.h5")
-        sonuc = DeepFace.analyze(file, actions = ['age'])
+        sonuc = DeepFace.analyze(img, actions = ['age'])
         age = model.predict(image)
         st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
         st.markdown("## yaşınız %i gibi görünüyor" %sonuc['age'])
