5cfd880e620e17276e8ac0881170097c48f7c918
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-02 01:57:59 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,23 +1,28 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
+import cv2
 
 def main():
-    st.header("How old are you according to a Convolution Neural Network? 🥸👧")
-    st.write("Upload an image of yourself below to find out! (preferably a squared image and containing only your face)")
+    st.header("Görünüşüne göre yaşını bil")
+    st.write("Öğrenmek için aşağıya kendi resminizi yükleyin! ")
     file = st.file_uploader("Upload Photo")
     if file is not None:
-        st.image(file, width=300)
+        st.image(file)
         image = Image.open(file)
-        image = tf.image.resize(image, [224,224]) 
-        image = tf.keras.preprocessing.image.img_to_array(image)
-        image = image / 255.0      
-        image = tf.expand_dims(image, axis=0)
+        # image = tf.image.resize(image, [224,224]) 
+        # image = tf.keras.preprocessing.image.img_to_array(image)
+        # image = image / 255.0      
+        # image = tf.expand_dims(image, axis=0)
+        
+        image = cv2.resize(image, (224, 224))
+        image = image.reshape(1, 224, 224, 3)
+        image = image/255.0
         
         model = tf.keras.models.load_model("my_model.h5")
         age = model.predict(image)
-        st.markdown("## You're %i years old according to our model!" %age[0][0])
-        st.write("Want to know how it works? Check out the [source code in GitHub](https://github.com/ubiratanfilho/age-gender-prediction)")
+        st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+        st.write("")
     
 if __name__ == '__main__':
 	main()
