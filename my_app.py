3a2cb346359e425f65509d66e51bdb3d5e36c520
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-02 02:18:51 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
+from deepface import DeepFace
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     if file is not None:
         st.image(file)
         image = Image.open(file)
+        img=image
         image = tf.image.resize(image, [224,224]) 
         image = tf.keras.preprocessing.image.img_to_array(image)
         image = image / 255.0      
@@ -19,9 +21,10 @@ def main():
       
         
         model = tf.keras.models.load_model("my_model.h5")
+        sonuc = DeepFace.analyze(img, actions = ['age'])
         age = model.predict(image)
         st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
-        st.write("")
+        st.markdown("## yaşınız %i gibi görünüyor" %sonuc['age'])
     
 if __name__ == '__main__':
 	main()
