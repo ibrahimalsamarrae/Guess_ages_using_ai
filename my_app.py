be662728f90da466c3f4fc08899fa6f3fa75a29a
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-02 16:08:52 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,30 +1,30 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
-from deepface import DeepFace
-import cv2
 
 def main():
-    st.header("Görünüşüne göre yaşını bil")
-    st.write("Öğrenmek için aşağıya kendi resminizi yükleyin! ")
-    file = st.file_uploader("Upload Photo")
-    if file is not None:
-        st.image(file)
-        image = Image.open(file)
-        img = cv2.imread(image)
-        image = tf.image.resize(image, [224,224]) 
-        image = tf.keras.preprocessing.image.img_to_array(image)
-        image = image / 255.0      
-        image = tf.expand_dims(image, axis=0)
         
-     
-      
-        
-        model = tf.keras.models.load_model("my_model.h5")
-        sonuc = DeepFace.analyze(img, actions = ['age'])
-        age = model.predict(image)
-        st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
-        st.markdown("## yaşınız %i gibi görünüyor" %sonuc['age'])
-    
+       st.header("Görünüşüne göre yaşını bil")
+       st.write("Öğrenmek için aşağıya kendi resminizi yükleyin! ")
+       file = st.file_uploader("Upload Photo")
+       if file is not None:
+            st.image(file, width=300)
+            image = Image.open(file)
+            
+            image = tf.keras.preprocessing.image.img_to_array(image)
+            image = tf.image.resize(image, [224,224]) 
+            image = image / 255.0      
+            image = tf.expand_dims(image, axis=0)
+            
+            model = tf.keras.models.load_model("my_model.h5")
+            age = model.predict(image)
+            
+            st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
+            
+            
 if __name__ == '__main__':
-	main()
+     main()
+        
+        
+        
+        
