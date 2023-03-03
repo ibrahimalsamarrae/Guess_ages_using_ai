f7a0af2859ab0b5bf8c5527f12a7e2ca9d295b60
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-03 20:40:45 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,27 +1,36 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.utils import load_img
 from PIL import Image
-import os 
+from deepface import DeepFace
 
 def main():
-      
+        
        st.header("Görünüşüne göre yaşını bil")
        st.write("Öğrenmek için aşağıya kendi resminizi yükleyin!")
        file = st.file_uploader("Upload Photo")
-       
        if file is not None:
-           file_details = {"FileName":file.name,"FileType":file.type}
-           st.write(file_details)
-           img = load_img(file)
-           st.image(img,height=250,width=250)
-           with open(os.path.join("https://github.com/ibrahimalsamarrae/ai_yas",file.name),"wb") as f:
-               f.write(file.getbuffer())         
-           st.success("Saved File") 
+            # st.image(file, width=300)
+            img = Image.open(file)
+            
+            # image = tf.keras.preprocessing.image.img_to_array(image)
+            # image = tf.image.resize(image, [224,224]) 
+            # image = image / 255.0      
+            # image = tf.expand_dims(image, axis=0)
+            # model = tf.keras.models.load_model("yasmodel.h5") 
+            # age = model.predict(image)
+            
+            # img = cv2.imread(file)
+            # img = cv2.resize(img, (224, 224))
+            # img = img.reshape(1, 224, 224, 3)
+            # img = img/255.0
+            # pred = model.predict(img)
+            objs = DeepFace.analyze(img, actions = ['age', 'gender', 'race', 'emotion'])
+            
+            
+            # st.markdown("## yaşınız %i gibi görünüyor" %pred[0][0])
+            st.markdown("## yaşınız %i gibi görünüyor" %objs['age'])
+            
             
 if __name__ == '__main__':
      main()
-        
-        
-        
         
