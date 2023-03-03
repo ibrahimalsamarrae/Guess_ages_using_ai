b421c672771f71f6d7815481a34dab0ebcb28120
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-03 20:31:11 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
-from deepface import DeepFace
 import os 
 
 def main():
@@ -17,27 +16,7 @@ def main():
            st.image(img,height=250,width=250)
            with open(os.path.join("https://github.com/ibrahimalsamarrae/ai_yas",file.name),"wb") as f:
                f.write(file.getbuffer())         
-           st.success("Saved File")
-       
-       
-       # if file is not None:
-            # st.image(file, width=300)
-            # img = Image.open(file)
-            
-            # image = tf.keras.preprocessing.image.img_to_array(image)
-            # image = tf.image.resize(image, [224,224]) 
-            # image = image / 255.0      
-            # image = tf.expand_dims(image, axis=0)
-            # model = tf.keras.models.load_model("yasmodel.h5") 
-            # age = model.predict(image)
-            
-   
-            # objs = DeepFace.analyze(img, actions = ['age', 'gender', 'race', 'emotion'])
-            
-            
-            # st.markdown("## yaşınız %i gibi görünüyor" %pred[0][0])
-            # st.markdown("## yaşınız %i gibi görünüyor" %objs['age'])
-            
+           st.success("Saved File") 
             
 if __name__ == '__main__':
      main()
