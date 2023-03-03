46687caab4cc60945e9897440ab36d1d3be7155d
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-03 20:54:34 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,36 +1,31 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
-from deepface import DeepFace
 
 def main():
         
        st.header("Görünüşüne göre yaşını bil")
-       st.write("Öğrenmek için aşağıya kendi resminizi yükleyin!")
+       st.write("Öğrenmek için aşağıya kendi resminizi yükleyin! ")
        file = st.file_uploader("Upload Photo")
        if file is not None:
-            # st.image(file, width=300)
-            img = Image.open(file)
+            st.image(file, width=300)
+            image = Image.open(file)
             
-            # image = tf.keras.preprocessing.image.img_to_array(image)
-            # image = tf.image.resize(image, [224,224]) 
-            # image = image / 255.0      
-            # image = tf.expand_dims(image, axis=0)
-            # model = tf.keras.models.load_model("yasmodel.h5") 
-            # age = model.predict(image)
+            image = tf.keras.preprocessing.image.img_to_array(image)
+            image = tf.image.resize(image, [224,224]) 
+            image = image.reshape(1, 224, 224, 3)
+            image = image / 255.0      
+            image = tf.expand_dims(image, axis=0)
             
-            # img = cv2.imread(file)
-            # img = cv2.resize(img, (224, 224))
-            # img = img.reshape(1, 224, 224, 3)
-            # img = img/255.0
-            # pred = model.predict(img)
-            objs = DeepFace.analyze(img, actions = ['age', 'gender', 'race', 'emotion'])
+            model = tf.keras.models.load_model("yasmodel.h5")
+            age = model.predict(image)
             
-            
-            # st.markdown("## yaşınız %i gibi görünüyor" %pred[0][0])
-            st.markdown("## yaşınız %i gibi görünüyor" %objs['age'])
+            st.markdown("## yaşınız %i gibi görünüyor" %age[0][0])
             
             
 if __name__ == '__main__':
      main()
+        
+        
+        
         
