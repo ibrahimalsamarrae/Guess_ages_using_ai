066c32fd0131b88461364726aa32a1007f0433f6
ibrahimalsamarrae <106987852+ibrahimalsamarrae@users.noreply.github.com>
2023-03-03 18:51:29 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import cv2
+from deepface import DeepFace
 
 def main():
         
@@ -10,24 +11,25 @@ def main():
        file = st.file_uploader("Upload Photo")
        if file is not None:
             # st.image(file, width=300)
-            img = Image.open(file)
+            # img = Image.open(file)
             
             # image = tf.keras.preprocessing.image.img_to_array(image)
             # image = tf.image.resize(image, [224,224]) 
             # image = image / 255.0      
             # image = tf.expand_dims(image, axis=0)
-            model = tf.keras.models.load_model("yasmodel.h5") 
+            # model = tf.keras.models.load_model("yasmodel.h5") 
             # age = model.predict(image)
             
-            img = cv2.imread(file)
-            img = cv2.resize(img, (224, 224))
-            img = img.reshape(1, 224, 224, 3)
-            img = img/255.0
-            pred = model.predict(img)
+            # img = cv2.imread(file)
+            # img = cv2.resize(img, (224, 224))
+            # img = img.reshape(1, 224, 224, 3)
+            # img = img/255.0
+            # pred = model.predict(img)
+            objs = DeepFace.analyze(img_path = "img4.jpg", actions = ['age', 'gender', 'race', 'emotion'])
             
             
-            
-            st.markdown("## yaşınız %i gibi görünüyor" %pred[0][0])
+            # st.markdown("## yaşınız %i gibi görünüyor" %pred[0][0])
+            st.markdown("## yaşınız %i gibi görünüyor" %objs[0][0])
             
             
 if __name__ == '__main__':
